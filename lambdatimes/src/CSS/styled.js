9a42e0278009eab085bf5f838c38a140372663ab
doanmade <43007082+doanmade@users.noreply.github.com>
2019-04-19 17:48:46 -0500

    working on styled-components and have a hard time with how everything goes together

---
+++ lambdatimes/src/CSS/styled.js
@@ -0,0 +1,16 @@
+// import React from 'react'
+
+const HeaderStyle = styled.header`
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  // width: 100%;
+  // height: 120px;
+  // margin-top: 44px;
+  // padding-bottom: 15px;
+`;
+
+const DataStyle = styled(data)`
+  margin-left: 25px;
+  flex: 1;
+`;
+export default styled;
--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
+
+const HeaderStyle = styled.div`
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  // width: 100%;
+  // height: 120px;
+  // margin-top: 44px;
+  // padding-bottom: 15px;
+`;
+
+const DataStyle = styled.div`
+  margin-left: 25px;
+  flex: 1;
+`;
+
+const TempStyle = styled.div`
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
-  )
-}
+    <HeaderStyle>
+      <div>
+        <DataStyle>SMARCH 32, 2018</DataStyle>
+        <h1>Lambda Times</h1>
+        <TempStyle>98°</TempStyle>
+      </div>
+    </HeaderStyle>
+  );
+};
 
-export default Header
+export default Header;
